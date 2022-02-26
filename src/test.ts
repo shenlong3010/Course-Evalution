a88ebdaaba7e31c7e50488aca701fa2f831cd794
Loc Lam <shenlong3010@gmail.com>
2022-02-26 16:27:32 -0500

    moved package, start test

--- src/test.ts
+++ src/test.ts
@@ -3,14 +3,14 @@ interface User {
     id: number;
   }
   
-  class UserAccount {
-    name: string;
+class UserAccount {
+	name: string;
     id: number
     constructor(name: string, id: number) {
       this.name = name;
       this.id = id;
     }
-  }
+}
   
 const user: User = new UserAccount("Murphy", 1);
 console.log(user);
