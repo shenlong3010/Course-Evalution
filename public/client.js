765b489f284cbe847c258ac94ac1d47d46273c50
ldv25 <ldv25@drexel.edu>
2022-02-28 18:33:34 -0500

    Update client.js

--- public/client.js
+++ public/client.js
@@ -4,14 +4,15 @@ var email = document.getElementById("email");
 var Course_id = document.getElementById("Course_id");
 var ques_num = document.getElementById("question_number");
 var table = document.getElementById("ques_list");
-var button = document.getElementById("submit");
-button.addEventListener("click", function () {
+var button1 = document.getElementById("number");
+var button2 = document.getElementById("submit");
+button1.addEventListener("click", function () {
     var num = document.getElementById("ques_num").value;
     var int_num = +num;
-    for (var i = 0; i < int_num; i++) {
-        var tableRow = document.createElement("tr");
+    for (var i = 0; i < 10; i++) {
+        //var tableRow = document.createElement("tr");
         console.log(i);
-        tableRow.textContent = "A";
-        table.append(tableRow);
+        //tableRow.textContent = "A";
+        //table.append(tableRow);
     }
 });
