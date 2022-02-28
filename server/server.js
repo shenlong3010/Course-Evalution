43aa2d81a3ea7b0e5400b29ab10d97672c6a77cc
ldv25 <ldv25@drexel.edu>
2022-02-28 18:27:56 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,11 +5,9 @@ app.use(express.json());
 app.use(express.static("public"));
 var port = 3000;
 var hostname = "localhost";
-
-// var env = require("../env.json");
-// var Pool = pg.Pool;
-// var pool = new Pool(env);
-
+// const env = require("../env.json")
+// const Pool = pg.Pool;
+// const pool = new Pool(env);
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
