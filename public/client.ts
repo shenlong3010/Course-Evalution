26c8cfd5a68f766f67926d8a3431ecf1ff47317f
ldv25 <ldv25@drexel.edu>
2022-03-11 19:20:50 -0500

    Update client.ts

--- public/client.ts
+++ public/client.ts
@@ -2,15 +2,27 @@ const firstName = document.getElementById("first_name");
 const lastName = document.getElementById("last_name");
 const email = document.getElementById("email");
 const Course_id = document.getElementById("Course_id");
-const ques_num = document.getElementById("question_number");
+
 const table = document.getElementById("ques_list");
 
-const button1 = document.getElementById("number");
+const button1 = document.getElementById("question_generate");
 const button2 = document.getElementById("submit");
+const message = document.getElementById("message");
 
 button1.addEventListener("click", function () {
-    var num = (<HTMLInputElement>document.getElementById("question_number")).value;
-    var int_num: number = +num;
+    const mcq_number = (<HTMLInputElement>document.getElementById("mcq_number")).value;
+    const cb_number = (<HTMLInputElement>document.getElementById("cb_number")).value;
+    const sa_number = (<HTMLInputElement>document.getElementById("sa_number")).value;
+
+    var int_mcq_num: number = +mcq_number;
+    var int_cb_num: number = +cb_number;
+    var int_sa_num: number = +sa_number;
+
+    console.log(int_mcq_num);
+    console.log(int_cb_num);
+    console.log(int_sa_num);
+
+    message.textContent = "" + int_mcq_num + ", " + int_cb_num + ", " + int_sa_num;
 
     for (let i = 0; i < 10; i++) {
         //var tableRow = document.createElement("tr");
