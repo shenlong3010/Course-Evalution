0dd84dfb0d389d56307ce5a4f50b23dea0cb0c08
ldv25 <ldv25@drexel.edu>
2022-03-11 19:32:58 -0500

    Update client.ts

--- public/client.ts
+++ public/client.ts
@@ -7,7 +7,12 @@ const table = document.getElementById("ques_list");
 
 const button1 = document.getElementById("question_generate");
 const button2 = document.getElementById("submit");
-const message = document.getElementById("message");
+
+const mcq_list = document.getElementById("mcq_list");
+const cbq_list = document.getElementById("cb_list");
+const saq_list = document.getElementById("sa_list");
+
+//const message = document.getElementById("message");
 
 button1.addEventListener("click", function () {
     const mcq_number = (<HTMLInputElement>document.getElementById("mcq_number")).value;
@@ -18,11 +23,7 @@ button1.addEventListener("click", function () {
     var int_cb_num: number = +cb_number;
     var int_sa_num: number = +sa_number;
 
-    console.log(int_mcq_num);
-    console.log(int_cb_num);
-    console.log(int_sa_num);
-
-    message.textContent = "" + int_mcq_num + ", " + int_cb_num + ", " + int_sa_num;
+    //message.textContent = "" + int_mcq_num + ", " + int_cb_num + ", " + int_sa_num;
 
     for (let i = 0; i < 10; i++) {
         //var tableRow = document.createElement("tr");
