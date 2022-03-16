6cbee49e4387c92caf833e878d5a4b935dee6096
ha635 <ha635@drexel.edu>
2022-03-16 19:39:29 -0400

    Update search.js

---
+++ public/search.js
@@ -0,0 +1,39 @@
+let button = document.getElementById("submit");
+let message = document.getElementById("message");
+let table = document.getElementById("survey");
+
+function removeChildren(element) {
+	while (element.hasChildNodes()) {
+		element.lastChild.remove();
+	}
+}
+
+// add Event Listener
+button.addEventListener("click", function() {
+
+    removeChildren(table);
+    message.textContent="";
+	let genre = dropdown.value;
+	fetch(`/search?course_id=${course_id}`).then(async function (response) {
+		console.log(response);
+		if (response.status === 200) {
+			return await response.json();
+		} else {
+			throw Error(response.status);
+		}
+	}).then(function (response) { 
+        for (let row of response.rows) {
+            let tableRow = document.createElement("tr");
+            for (let key of ["question", "options"]) {
+                let cell = document.createElement("td");
+                cell.textContent = row[key];
+                tableRow.append(cell);
+            }
+            table.append(tableRow);
+        }
+	}).catch(function (error) {
+        message.textContent="No course find";
+		console.log(error);
+	});
+
+});
