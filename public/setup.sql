677b1914b08ceb5a1563beccc1385408b7c29ba8
ha635 <ha635@drexel.edu>
2022-03-16 19:55:26 -0400

    Update setup.sql

--- public/setup.sql
+++ public/setup.sql
@@ -18,3 +18,17 @@ CREATE TABLE course(
     PRIMARY KEY (q_id),
     FOREIGN KEY (course_id) references faculty(course_id)
     );
+
+
+/*
+
+- insert values into tables
+insert into faculty values ('Galen', 'long', 'abc@drexel.edu', 'CS375');
+insert into faculty values ('Galen3', 'long1', 'abc123@drexel.edu', 'CS275');
+
+insert into course values (1, 'CS375', 'Question 1', 'A', '{ "option": "A", "list": {"A": "option A","B": "option B", "C": "option C"}, "response": "B"}');
+
+
+- join a table
+F.l_name, F.f_name, F.course_id, C.question, C.q_type, C.info from faculty AS F inner join course AS C on F.course_id = C.course_id;
+*/
