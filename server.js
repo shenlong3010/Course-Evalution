b088af494de72cfdc170d2c13c421524b2d1358b
ha635 <ha635@drexel.edu>
2022-03-16 20:34:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -30,3 +30,53 @@ app.get('/', function (req, res) {
 app.listen(port, hostname, function () {
     return console.log("Listening at: http://" + hostname + ":" + port);
 });
+
+app.post('/add', jsonParser, function(req, res){
+    let course = req.body;
+    console.log(course);
+
+    const query = "INSERT INTO course(q_id, course_id, question, q_type, info) VALUES($1, $2, $3, $4, $5) RETURNING *";
+    let values = [course.q_id, course.course_id, course.question, course.q_type , course.info];
+
+    pool.query(query, values)
+    .then(function (response) {
+        console.log(response.rows);
+        return res.send();
+    })
+    .catch(function (error) {
+        return res.sendStatus(500);
+    });
+});
+
+
+app.get("/search", function (req, res) {
+    let { course_id } = req.query;
+
+    if(course_id.length === 0){
+        pool.query("SELECT * FROM course")
+        .then(function (response) {
+            if(response.rows.length === 0){
+                return res.sendStatus(404);
+            }
+            return res.json({ rows: response.rows });
+        })
+        .catch(function (error) {
+            return res.sendStatus(500);
+        });
+    }else {
+        pool.query("SELECT * FROM course WHERE course_id = $1", [course_id])
+            .then(function (response) {
+                if(response.rows.length === 0){
+                    return res.sendStatus(404);
+                }
+                return res.json({ rows: response.rows });
+            })
+            .catch(function (error) {
+                return res.sendStatus(500);
+            });
+        }
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Listening at: http://${hostname}:${port}`);
+});
